a0783f5223e300fc6ce337f145978ad3a6dd9c77
Costas Basdekis <costas.basdekis@gmail.com>
2017-03-11 15:49:20 +0000

    Add center tool

--- js/demo.js
+++ js/demo.js
@@ -9,6 +9,41 @@ class Demo {
             new CartesianPoint(522, 522));
 
         paper.setup(this.canvas);
+
+        this.centerTool = this.createCenterTool();
+        this.paint();
+    }
+
+    createCenterTool() {
+        const tool = new paper.Tool();
+
+        tool.init = Demo.initCenterTool;
+        tool.onMouseDown = tool.onMouseDrag = Demo.onMouseDragCenterTool;
+        tool.demo = this;
+        tool.init();
+
+        return tool;
+    }
+
+    static initCenterTool() {
+        this.centerPath = new paper.Path.Circle({x: 50, y: 50}, 15);
+        this.centerPath.strokeColor = 'red';
+        this.centerPath.fillColor = 'yellow';
+    }
+
+    static onMouseDragCenterTool(toolEvent) {
+        this.centerPath.position = {
+            x: toolEvent.event.layerX,
+            y: toolEvent.event.layerY,
+        };
+        this.demo.onMouseDrag(toolEvent);
+    }
+
+    onMouseDrag(toolEvent) {
+        //
+    }
+
+    paint() {
         this.lines.paint();
     }
 }
