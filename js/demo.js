b45b120727f5734718e7012fd67abb7ddab82831
Costas Basdekis <costas.basdekis@gmail.com>
2017-03-17 08:18:31 +0000

    Add FPS counter

--- js/demo.js
+++ js/demo.js
@@ -1,11 +1,12 @@
 "use strict";
 
 class Demo {
-    constructor(canvas, roomsElement, xElement, yElement) {
+    constructor(canvas, roomsElement, xElement, yElement, fpsElement) {
         this.canvas = canvas;
         this.roomsElement = roomsElement;
         this.xElement = xElement;
         this.yElement = yElement;
+        this.fpsElement = fpsElement;
 
         paper.setup(this.canvas);
 
@@ -14,13 +15,11 @@ class Demo {
 
         this.center = {x: 50, y: 50};
 
+        this.frameTimestamps = [];
+
         this.createRoom(MazeRoom);
         this.updateRays();
         this.centerTool = this.createCenterTool(this.center);
-
-        this.paint();
-
-        this.onMouseDrag({event: {layerX: this.center.x, layerY: this.center.y}});
 
         Rooms.populateComboBox(roomsElement);
         this.roomsElement.onchange = this.roomsOnChange.bind(this);
@@ -69,12 +68,16 @@ class Demo {
     }
 
     updateRays() {
+        const start = new Date().getTime();
+        this.calculateFPS();
         this.rays.fromCartesianLines(this.center, this.walls);
         this.rays.simplify();
 
         this.rays.updatePath(this.center);
         this.rays.path.strokeColor = 'yellow';
         this.rays.path.fillColor = 'green';
+        const end = new Date().getTime();
+        this.calculateFPS(end - start);
     }
 
     createRoom(room) {
@@ -84,12 +87,31 @@ class Demo {
         this.walls.path.strokeColor = 'blue';
     }
 
-    paint() {
-        paper.view.draw();
-    }
-
     updateCoordsDisplay() {
         this.xElement.textContent = this.center.x;
         this.yElement.textContent = this.center.y;
+    }
+
+    calculateFPS(newTime) {
+        if (!newTime) {
+            return;
+        }
+
+        this.frameTimestamps.push(newTime);
+        while (this.frameTimestamps.length > 150) {
+            this.frameTimestamps.shift();
+        }
+
+        const totalMilliseconds = this.frameTimestamps.reduce(
+            (total, current) => total + current, 0);
+        const newFramerate =
+            this.frameTimestamps.length / (totalMilliseconds / 1000);
+        if (this.frameRate) {
+            this.frameRate = (this.frameRate * 9 + newFramerate) / 10;
+        } else {
+            this.frameRate = newFramerate;
+        }
+        this.frameRate = Math.round(this.frameRate);
+        this.fpsElement.textContent = this.frameRate;
     }
 }
